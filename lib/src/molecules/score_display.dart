ae6426036e29d355e0bfeb41f3cafbb10ba2ae2e
Dylan Modell <dylan.modell.promo@gmail.com>
2019-02-03 01:05:33 -0800

    physics working, score*100, added difficulty var

--- lib/src/molecules/score_display.dart
+++ lib/src/molecules/score_display.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
 
 class ScoreDisplay extends StatelessWidget {
+  final double score;
+
+  ScoreDisplay(this.score);
+
   @override
   Widget build(BuildContext context) {
     return Positioned(
-      child: Text('200'),
+      top: 100 + score,
+      child: Center(
+        child: Text('${(score*100).round()}',
+          style: TextStyle(fontSize: 44),
+        ),
+      ),
     );
   }
 }
--- lib/src/organisms/playing_field.dart
+++ lib/src/organisms/playing_field.dart
@@ -1,16 +1,85 @@
 import 'package:flutter/material.dart';
 import '../molecules/score_display.dart';
+import 'dart:math' as math;
 
-class PlayingField extends StatelessWidget {
+class PlayingField extends StatefulWidget {
+  _PlayingFieldState createState() => _PlayingFieldState();
+}
+
+class _PlayingFieldState extends State<PlayingField>
+  with TickerProviderStateMixin{
+
+  Animation<double> scoreReducer;
+  AnimationController scoreReducerController;
+
+  double score;
+  double difficulty;
+
+  @override
+  void initState() {
+    score = 0.0;
+    difficulty = 0.8; //TODO: Tune difficulty 0.1 is easy mode for dev
+
+    scoreReducerController = AnimationController(
+      duration: Duration(
+        milliseconds: math.max( (400 - difficulty*300).round(), 50 ) 
+      ),
+      vsync: this
+    );
+    scoreReducer = Tween(begin: 1.0, end: 0.0).animate(
+      CurvedAnimation(
+        parent: scoreReducerController,
+        curve: Curves.linear
+      )
+    );
+
+    super.initState();
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Container(
-      alignment: Alignment(0, 0),
-      child: Stack(
-        children: <Widget>[
-          ScoreDisplay(),
-        ],
+    Size size = MediaQuery.of(context).size;
+
+    return GestureDetector(
+      onVerticalDragStart:(details) => _placeFinger(details),
+      onVerticalDragUpdate: (details) => _onDrag(details),
+      onVerticalDragEnd: (details) => _liftFinger(details),
+      child: Container(
+        alignment: AlignmentDirectional.center,
+        width: size.width,
+        height: size.height,
+        color: Colors.yellow,
+        child: Stack(
+          alignment: AlignmentDirectional.center,
+          children: <Widget>[
+            AnimatedBuilder(
+              animation: scoreReducer,
+              builder: (conext, child){
+                score *= scoreReducer.value;
+                return ScoreDisplay(score);
+              }
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  void _liftFinger(DragEndDetails details) => _youLose();
+
+  void _placeFinger(DragStartDetails details){
+    scoreReducerController.reset();
+  }
+
+  void _onDrag(DragUpdateDetails details){
+    double punishment = score + details.delta.dy/((difficulty*10).round());
+    double newScore = score + details.delta.dy/(math.pow(score, difficulty) + 1);
+    if(newScore >= score) setState(() => score = newScore);
+    else if(punishment >= 0) setState(()=> score = punishment);
+    else setState(()=> score = 0);
+  }
+
+  void _youLose(){
+    scoreReducerController.forward();
   }
 }
